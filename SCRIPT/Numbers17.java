9a2d3f355220069be0a7408892adbe07c5321f45
uhamhz <ammarhafizh2404@gmail.com>
2023-10-31 18:40:40 +0700

    Add New Files

---
+++ SCRIPT/Numbers17.java
@@ -0,0 +1,20 @@
+package SCRIPT;
+
+import java.util.Arrays;
+
+public class Numbers17 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang Baris ke-" + (i+1) + " : " + myNumbers[i].length);
+        }
+    }
+}
